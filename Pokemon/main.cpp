f032816b7ba06ebc8e89a8bb1eab20f60190f30a
ivestheprogrammer <littlebirdcanflyhigh@gmail.com>
2025-07-04 19:07:03 -0400

    commit 1
    Update .gitignore and enhance main.cpp functionality
    
    - Added Visual Studio-related entries to .gitignore to prevent tracking of project-specific files.
    - Noted changes in the binary file `Browse.VC.db`.
    - Enhanced `main.cpp` with user interaction: prompts for the player's name and provides a personalized message.

--- Pokemon/main.cpp
+++ Pokemon/main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+using namespace std;
 
 int main() {
+    string playerName;
+
+    cout << "What's your name? \n";
+    cin >> playerName;
+
+    cout << "\nWelcome " << playerName << "! Are you ready to start your adventure?" << endl;
 
     return 0;
 }
